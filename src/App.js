c1fd41eb406167038631bc62b11e25f1f6225311
Lucas Tavares <lucas.lucas2828@gmail.com>
2021-03-12 21:24:07 -0300

    Last css tweak

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
       <div className="m-auto flex flex-col">
         <Quotation params={{ author, quote, isLoaded }} />
         <button
-          className="absolute text-lg md:text-base xl:text-5xl  bottom-0 left-0 md:left-2/3 md:bottom-1/4 w-full h-24 md:h-12 md:w-36 xl:w-96 xl:h-36 bg-white hover:bg-gray-100 text-gray-800 font-semibold md:py-2 md:px-4 py-1 px-2 border border-gray-400 rounded shadow focus:outline-none"
+          className="absolute text-lg md:text-base xl:text-5xl bottom-0 inset-x-0 md:left-2/3 md:bottom-1/4 w-full h-24 md:h-12 md:w-36 xl:w-96 xl:h-36 bg-white hover:bg-gray-100 text-gray-800 font-semibold md:py-2 md:px-4 py-1 px-2 border border-gray-400 rounded shadow focus:outline-none"
           onClick={handleOnClick}
         >
           New Quote
